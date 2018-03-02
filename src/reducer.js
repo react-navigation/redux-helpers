ffa602a785ba3f2984f884968c80c38e2a20f100
ashoat <ashoat@gmail.com>
2018-03-01 20:58:55 -0500

    Don't return initialState if router returns null

--- src/reducer.js
+++ src/reducer.js
@@ -6,17 +6,20 @@ import type { Navigator, ReducerState } from './types'
 
 import { NavigationActions } from "react-navigation";
 
-export function createNavigationReducer(navigator: Navigator): Reducer<*, *> {
+function createNavigationReducer(navigator: Navigator): Reducer<*, *> {
   const initialState = navigator.router.getStateForAction(
     NavigationActions.init(),
-    null
+    null,
   );
 
   return (
     state: ReducerState = initialState,
     action: NavigationAction
   ): ReducerState => {
-    const newState = navigator.router.getStateForAction(action, state);
-    return newState || state;
+    return navigator.router.getStateForAction(action, state);
   };
+};
+
+export {
+  createNavigationReducer,
 };
