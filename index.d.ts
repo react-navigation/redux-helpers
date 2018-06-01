8cf71e0e05d5f59dce05a49bdc477e53cd281f70
Vladyslav Tserman <vladjan@gmail.com>
2018-05-31 17:44:11 -0700

    Add `createNavigationPropConstructor` and `createDidUpdateCallback` functions into Type definitions for TypeScript

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,12 @@
 declare module 'react-navigation-redux-helpers' {
-    import { NavigationContainer, NavigationEventCallback, NavigationEventSubscription, NavigationState } from 'react-navigation';
+    import {
+        NavigationContainer,
+        NavigationEventCallback,
+        NavigationEventSubscription,
+        NavigationState,
+        NavigationDispatch,
+        NavigationScreenProp
+    } from 'react-navigation';
     import { Middleware, Reducer } from 'redux';
 
     export type Navigator = NavigationContainer;
@@ -11,8 +18,15 @@ declare module 'react-navigation-redux-helpers' {
     export function createReactNavigationReduxMiddleware<S>
     (key: string, navStateSelector: (state: S) => NavigationState): Middleware;
 
+    export function createDidUpdateCallback(key: string): () => void;
+
     export function createReduxBoundAddListener
     (key: string): (eventName: string, callback: NavigationEventCallback) => NavigationEventSubscription;
 
     export function createNavigationReducer(navigator: Navigator): Reducer<ReducerState>;
+
+    export function createNavigationPropConstructor(key: string): (
+        dispatch: NavigationDispatch,
+        state: NavigationState,
+    ) => NavigationScreenProp<NavigationState>
 }
