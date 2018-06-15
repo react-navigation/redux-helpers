804f9f80e9f9d74b58fd8d09e4b07ad7ba9b67df
Alexis Mangin <me@alexismangin.com>
2018-06-15 13:19:13 +0100

    Add reduxify to types (#40)
    * Add reduxify to types
    
    * Add missing peerDependencies: redux
    
    * Remove outdated methods from type definition
    
    * Remove outdated methods from type definition

--- index.d.ts
+++ index.d.ts
@@ -7,26 +7,20 @@ declare module 'react-navigation-redux-helpers' {
     NavigationDispatch,
     NavigationScreenProp
   } from 'react-navigation';
-  import { Middleware, Reducer } from 'redux';
+  import React from 'react';
+  import { Middleware, Reducer, Dispatch } from 'redux';
 
   export type Navigator = NavigationContainer;
 
   export type ReducerState = NavigationState | null | undefined;
 
-  export function initializeListeners(key: string, state: NavigationState): void;
-
   export function createReactNavigationReduxMiddleware<S>
   (key: string, navStateSelector: (state: S) => NavigationState): Middleware;
 
-  export function createDidUpdateCallback(key: string): () => void;
-
-  export function createReduxBoundAddListener
-  (key: string): (eventName: string, callback: NavigationEventCallback) => NavigationEventSubscription;
-
   export function createNavigationReducer(navigator: Navigator): Reducer<ReducerState>;
 
-  export function createNavigationPropConstructor(key: string): (
-    dispatch: NavigationDispatch,
-    state: NavigationState,
-  ) => NavigationScreenProp<NavigationState>
+  export function reduxifyNavigator(
+    navigator: Navigator,
+    key: string,
+  ): React.ComponentType<{ state: NavigationState; dispatch: Dispatch }>;
 }
